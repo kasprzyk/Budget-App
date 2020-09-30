4b797753bda06873dbc0ffdd7a1ee9ce4ae5c794
kasprzyk <piotkas1@gmail.com>
2020-09-30 09:33:45 +0200

    ES6 Array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -26,3 +26,16 @@ const book = {
 const { name: publisherName = 'Self-Published' } = book.publisher;
 
 console.log(publisherName);
+
+//
+// Array destructuring
+//
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);

db04745f193cfc3cf894c5405a338353a115006c
kasprzyk <piotkas1@gmail.com>
2020-10-29 13:54:11 +0100

    Setting up datePicker

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
+//const date = new Date()
+const now = moment();
 export default class ExpenseForm extends React.Component {
   state = {
     description: '',
     note: '',
     amount: '',
+    createdAt: moment(),
+    calendarFocused: false,
   };
   onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(() => ({
       description,
     }));
+  };
+
+  onDateChange = (createdAt) => {
+    this.setState(() => ({ createdAt }));
+  };
+
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
   };
 
   onAmountChange = (e) => {
@@ -44,7 +59,16 @@ export default class ExpenseForm extends React.Component {
             type='number'
             placeholder='Amount'
             value={this.state.amount}
+            onChange={this.onAmountChange}
           ></input>
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+          />
           <textarea
             placeholder='Add a note to your description (optional)'
             value={this.state.note}

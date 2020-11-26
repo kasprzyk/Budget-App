6b8eb8643b4812167d6eaead3b1dc1637fc3b1bd
kasprzyk <piotkas1@gmail.com>
2020-11-26 18:31:28 +0100

    fix

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  //expect(wrapper.find('h1').text()).toBe('Expensify');
-  expect(toJSON(wrapper)).toMatchSnapshot();
-
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
-  //console.log(renderer.getRenderOutput());
+  expect(wrapper).toMatchSnapshot();
 });

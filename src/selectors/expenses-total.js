4ef1682116d68d82425e043e69efac8be0d8c9a6
kasprzyk <piotkas1@gmail.com>
2020-12-03 18:19:38 +0100

    Adding summary component

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,11 +1,7 @@
 export default (expenses) => {
-  if (expenses.length === 0) {
-    return 0;
-  } else {
-    return expenses
-      .map((expense) => expense.amount)
-      .reduce((a, b) => {
-        return a + b;
-      }, 0);
-  }
+  return expenses
+    .map((expense) => expense.amount)
+    .reduce((a, b) => {
+      return a + b;
+    }, 0);
 };

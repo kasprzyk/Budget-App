84a06b8ef9e74e51d085e9cc9a49e359f5520ee9
kasprzyk <piotkas1@gmail.com>
2020-12-23 13:03:51 +0100

    Styling login page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -19,7 +19,7 @@ const AppRouter = () => (
         <PrivateRoute path='/dashboard' component={ExpenseDashboardPage} />
         <PrivateRoute path='/create' component={AddExpensePage} />
         <PrivateRoute path='/edit/:id' component={EditExpensePage} />
-        <Route component={NotFoundPage} />
+        <Route component={LoginPage} />
       </Switch>
     </div>
   </Router>

dcbff480ab69e12bd13e258380fd090bad9bfc3f
kasprzyk <piotkas1@gmail.com>
2020-12-23 13:40:27 +0100

    Styling buttons

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,9 @@ export const LoginPage = ({ startLogin }) => (
   <div className='box-layout'>
     <div className='box-layout__box'>
       <h1 className='box-layout__title'>Expensify App</h1>
-      <button onClick={startLogin}>Login</button>
+      <button className='button' onClick={startLogin}>
+        Login with google
+      </button>
     </div>
   </div>
 );

88627f6f5c7bc4264da4ba94753908ee0bc513e2
kasprzyk <piotkas1@gmail.com>
2020-12-24 14:33:38 +0100

    babel-polyfill

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { DateRangePicker } from 'react-dates';
+import 'react-dates/initialize';
 import {
   setTextFilter,
   sortByDate,
